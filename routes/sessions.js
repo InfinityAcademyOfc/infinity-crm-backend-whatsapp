e4b723cc7b8e9175e1ab3e4b773110542beeebb9
InfinityAcademyOfc <infinityacademyofc@gmail.com>
2025-05-02 21:32:45 -0300

    Update sessions.js

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,8 +1,15 @@
-// routes/sessions.js
 const express = require('express');
 const router = express.Router();
-const { getQRCode } = require('../controllers/whatsappController');
+const { startSession, getQRCode } = require('../controllers/whatsappController');
 
+// Iniciar uma sessão do WhatsApp
+router.post('/:id/start', (req, res) => {
+  const { id } = req.params;
+  startSession(id);
+  res.status(200).json({ message: `Sessão ${id} iniciada.` });
+});
+
+// Obter o QR Code da sessão
 router.get('/:id/qrcode', getQRCode);
 
 module.exports = router;
